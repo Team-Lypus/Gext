dedb993684f19fd3c1b4d069f001ca8bd9b19e5b
Abraham Leiro Fernández <abrahamleirofernandez@gmail.com>
2023-06-05 01:54:35 +0200

    EMPTY BODY ERROR CATCH

--- src/controllers/articles.js
+++ src/controllers/articles.js
@@ -1,49 +1,57 @@
-const pool = require("./database")
+const pool = require("./database");
 
 class Articles {
-    list = async (req, res) => {
-        const resp = await pool.query("SELECT * FROM articles");
-        res.json(resp.rows);
-    }
+  list = async (req, res) => {
+    const resp = await pool.query("SELECT * FROM articles");
+    res.json(resp.rows);
+  };
 
-    get = async (req, res) => {
-        const resp = await pool.query("SELECT * FROM articles WHERE id = $1", [req.params.id]);
-        res.json(resp.rows[0]);
-    }
+  get = async (req, res) => {
+    const resp = await pool.query("SELECT * FROM articles WHERE id = $1", [
+      req.params.id,
+    ]);
+    res.json(resp.rows[0]);
+  };
 
-    create = async (req, res) => {
-        const { nombre, descripcion, precio, iva } = req.body;
-        await pool.query(`INSERT INTO public.articles(
+  create = async (req, res) => {
+    const { nombre, descripcion, precio, iva } = req.body;
+    await pool.query(`INSERT INTO public.articles(
             nombre, descripcion, iva, precio)
-            VALUES ('${nombre}', '${descripcion}', ${iva}, ${precio});`)
-        res.json({
-            message: "articulo creado"
-        })
+            VALUES ('${nombre}', '${descripcion}', ${iva}, ${precio});`);
+    res.json({
+      message: "articulo creado",
+    });
+  };
+
+  delete = async (req, res) => {
+    const id = req.params.id;
+    await pool.query(`DELETE FROM public.articles WHERE id = ${id}`);
+    res.json({
+      message: `articulo ${id} eliminado`,
+    });
+  };
+
+  update = async (req, res) => {
+    if (req.body == {}) {
+      res.json({
+        message: "EMPTY BODY",
+      });
+      return;
     }
+    const id = req.params.id;
+    let sql = "UPDATE articles SET ";
 
-    delete = async (req, res) => {
-        const id = req.params.id;
-        await pool.query(`DELETE FROM public.articles WHERE id = ${id}`);
-        res.json({
-            message: `articulo ${id} eliminado`
-        })
+    for (const [key, value] of Object.entries(req.body)) {
+      sql += `${key} = '${value}',`;
     }
+    sql = sql.substring(0, sql.length - 1);
 
-    update = async (req, res) => {
-        const id = req.params.id
-        let sql = "UPDATE articles SET ";
-
-        for (const [key, value] of Object.entries(req.body)){
-            sql += `${key} = '${value}',`
-        }
-        sql = sql.substring(0, sql.length - 1)
-
-        sql += ` WHERE id = ${id};`;
-        await pool.query(sql);
-        res.json({
-            message: `articulo ${id} actualizado`,
-        })
-    }
+    sql += ` WHERE id = ${id};`;
+    await pool.query(sql);
+    res.json({
+      message: `articulo ${id} actualizado`,
+    });
+  };
 }
 
-module.exports = Articles
+module.exports = Articles;
--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,48 +1,61 @@
 const pool = require("./database");
 
 class Users {
+  list = async (req, res) => {
+    const response = await pool.query("SELECT id, username FROM users");
+    res.status(200).json(response.rows);
+  };
 
-    list = async (req, res) =>  {
-        const response = await pool.query("SELECT id, username FROM users");
-        res.status(200).json(response.rows)
+  get = async (req, res) => {
+    const response = await pool.query(
+      "SELECT id, username FROM users WHERE id = $1",
+      [req.params.id]
+    );
+    res.json(response.rows);
+  };
+
+  create = async (req, res) => {
+    const { username, password } = req.body;
+    const response = await pool.query(
+      "INSERT INTO users (username, password) VALUES ($1, $2)",
+      [username, password]
+    );
+    res.json({
+      status: 200,
+      message: "usuario creado",
+    });
+  };
+
+  delete = async (req, res) => {
+    const response = await pool.query("DELETE FROM users WHERE id = $1", [
+      req.params.id,
+    ]);
+    res.json({
+      message: `usuario ${req.params.id} eliminado`,
+    });
+  };
+
+  update = async (req, res) => {
+    if (req.body == {}) {
+      res.json({
+        message: "EMPTY BODY",
+      });
+      return;
     }
+    const id = req.params.id;
+    let sql = "UPDATE users SET ";
 
-    get = async (req, res)=>{
-        const response = await pool.query("SELECT id, username FROM users WHERE id = $1", [req.params.id]);
-        res.json(response.rows)
+    for (const [key, value] of Object.entries(req.body)) {
+      sql += `${key} = '${value}',`;
     }
+    sql = sql.substring(0, sql.length - 1);
 
-    create = async (req, res)=>{
-        const { username, password } = req.body;
-        const response = await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [username, password]);
-        res.json({
-            status: 200,
-            message: 'usuario creado'
-        })
-    }
-
-    delete = async (req, res)=>{
-        const response = await pool.query("DELETE FROM users WHERE id = $1", [req.params.id]);
-        res.json({
-            message: `usuario ${req.params.id} eliminado`
-        })
-    }
-
-    update = async (req, res)=>{
-        const id = req.params.id;
-        let sql = "UPDATE users SET ";
-
-        for (const [key, value] of Object.entries(req.body)){
-            sql += `${key} = '${value}',`
-        }
-        sql = sql.substring(0, sql.length - 1)
-
-        sql += ` WHERE id = ${id};`;
-        await pool.query(sql);
-        res.json({
-            message: `usuario ${id} actualizado`
-        })
-    }
+    sql += ` WHERE id = ${id};`;
+    await pool.query(sql);
+    res.json({
+      message: `usuario ${id} actualizado`,
+    });
+  };
 }
 
-module.exports =  Users;
+module.exports = Users;
